1a1cfb2be1fdaef21480628d219d2852f4394156
kalistan <kaliesha.pickering@gmail.com>
2018-08-04 13:44:51 -0400

    jquery

--- assets/articles.js
+++ assets/articles.js
@@ -1,13 +1,13 @@
-var Url = "https://api.nytimes.com/svc/books/v3/lists//.json?";
-queryUrl = url += '?' + $.param({
+var url = "https://api.nytimes.com/svc/books/v3/lists//.json?";
+var queryUrl = url += '?' + $.param({
     'api-key': "4983d35a5eb44663a93612a5e1be9d39"
   });
 
 $.ajax({
     url: queryUrl,
-    method: 'GET'
-}).then(function(response){
-
+    method: "GET"
+}).then(function(response){ 
+console.log(response);
 }).catch(function(error){
     console.log(error);
-})
+});
